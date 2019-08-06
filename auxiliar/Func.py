9aeec62ba557ffb304bc3ed1173462d1035fea45
Luiz H. D. S. Bispo <luizh3bispo@gmail.com>
2019-08-06 19:22:53 -0300

    Func.py
    Added the statement fitgaussian.

--- auxiliar/Func.py
+++ auxiliar/Func.py
@@ -54,3 +54,12 @@ def moments(data):
     width_y = np.sqrt(np.abs((np.arange(row.size)-x)**2*row).sum()/row.sum())
     height = data.max()
     return height, x, y, width_x, width_y
+
+def fitgaussian(data):
+    """Returns (height, x, y, width_x, width_y)
+    the gaussian parameters of a 2D distribution found by a fit"""
+    params = moments(data)
+    errorfunction = lambda p: np.ravel(gaussian(*p)(*np.indices(data.shape)) -
+                                 data)
+    p, success = optimize.leastsq(errorfunction, params)
+    return p
